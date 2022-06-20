7376cf9d1716a3d6c089cdad7b4b52ca4e70b6ee
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2022-06-19 18:35:36 -0700

    Update timer.js

--- src/timer.js
+++ src/timer.js
@@ -5,13 +5,27 @@ class Timer extends React.Component {
         super(props);
 
         this.state = {
-            timeRemaining: 0
+            timeRemaining: 0,
+            workTime: 0,
+            shortBreakTime: 0,
+            longBreakTime: 0,
         }
 
+        this.toggleTimerOptions = this.toggleTimerOptions.bind(this);
+        this.updateDisplay = this.updateDisplay.bind(this);
         this.toggleTimer = this.toggleTimer.bind(this);
         this.resetTimer = this.resetTimer.bind(this);
-        this.editTimer = this.editTimer.bind(this);
         this.setTimer = this.setTimer.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({
+            interval: setInterval(() => {
+                this.setState( prevState => {
+                    timeRemaining: prevState.timeRemaining - 1
+                },this.updateDisplay)
+            }, 1000)
+        })
     }
 
     toggleTimer() {
@@ -29,22 +43,49 @@ class Timer extends React.Component {
         alert("Reset!");
     }
 
-    editTimer() {
-        alert("Edit!");
+    toggleTimerOptions() {
+        let inputContainer = $(".input-container");
+
+        if(inputContainer.css("visibility") == "visible") {
+            inputContainer.css("visibility", "hidden");
+        }
+        else {
+            inputContainer.css("visibility", "visible");
+        }
+    
     }
 
     setTimer() {
+        let pomodoroInput = $(".pomodoro-input");
+        let shortBreakInput = $(".short-break-input");
+        let longBreakInput = $(".long-break-input");
+
+        this.setState({
+            timeRemaining: pomodoroInput.val()*60,
+            workTime: pomodoroInput.val()*60,
+            shortBreakTime: shortBreakInput.val()*60,
+            longBreakTime: longBreakInput.val()*60
+        }, this.updateDisplay);
+
+        this.toggleTimerOptions();
         
+    }
+
+    updateDisplay() {
+        let display = $(".timer-display");
+
+        display.html(`${this.state.timeRemaining/60}:${this.state.timeRemaining % 60}`);
     }
 
     render() {
         return (
             <div>
-                <h1 className="timer-display text-center display-1">00:00:00</h1>
-                <div class="container d-flex flex-row justify-content-center">
-                    <input className="text-center d-print-inline-block" type="text" placeholder="00"></input>:
-                    <input className="text-center d-print-inline-block" type="text" placeholder="00"></input>:
-                    <input className="text-center d-print-inline-block" type="text" placeholder="00"></input>
+                <h1 className="timer-display text-center display-1">00:00</h1>
+                <div className="input-container container d-flex flex-row justify-content-center">
+                    <input className="pomodoro-input" type="text" placeholder="Pomodoro Duration (min)"></input>
+                    <input className="short-break-input" type="text" placeholder="Short Break Duration (min)"></input>
+                    <input className="long-break-input" type="text" placeholder="Long Break Duration (min)"></input>
+                    <button className="btn btn-primary save-edits-btn" onClick={this.setTimer}>Save</button>
                 </div>
                  
                 <div className="container d-flex flex-row justify-content-center">
@@ -55,7 +96,7 @@ class Timer extends React.Component {
                         <button className="btn btn-primary" onClick={this.resetTimer}>Reset</button>
                     </div>
                     <div className="col-md-1">
-                        <button className="btn btn-primary" onClick={this.editTimer}>Edit</button>
+                        <button className="btn btn-primary" onClick={this.toggleTimerOptions}>Edit</button>
                     </div>
                 </div>
             </div>
